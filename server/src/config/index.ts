8d3b911fe9948a61fd52689d26d11db34b854991
Akalanka Perera <akalankaperera128@gmail.com>
2025-04-23 22:54:05 +0530

    feat: clusterized server

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -3,57 +3,53 @@ import { z } from '@sliit-foss/zelebrate';
 
 const logger = moduleLogger('Config');
 
-interface IConfig<T> {
-  schema: Record<string, z.ZodTypeAny>;
-  values: T;
+export enum Environment {
+  Production = 'production',
+  Development = 'development',
+  Local = 'local'
 }
 
 /**
  * @description Validates a set of values against the given schema. If validation fails, the process exits with code 1 with an error message.
  * @returns The validated config values
  */
-const validate = <T>({ schema, values }: IConfig<T>) => {
+const validate = <T extends z.ZodRawShape>({
+  schema,
+  values
+}: {
+  schema: z.ZodObject<T>;
+  values: Record<string, string | undefined>;
+}) => {
   try {
-    return z.object(schema).parse(values);
+    return schema.parse(values);
   } catch (e) {
     logger.error(`Environment validation failed. \nDetails - ${e}\nExiting...`);
     process.exit(1);
   }
 };
 
-export enum Environment {
-  Production = 'prod',
-  Development = 'dev',
-  Local = 'local'
-}
-
 const config = validate({
-  schema: {
-    HOST: z.string().optional(),
-    PORT: z.number().optional(),
+  schema: z.object({
+    HOST: z.string().default('0.0.0.0'),
+    PORT: z.number().default(8080),
+    CLUSTER_SIZE: z.coerce.number().optional(),
     DB_URL: z.string(),
     REDIS_CONNECTION_STRING: z.string(),
-    JWT_SECRET: z.string().optional(),
-    SALT_ROUNDS: z.number().optional(),
-    ACCESS_TOKEN_EXPIRY: z.string().optional(),
-    REFRESH_TOKEN_EXPIRY: z.string().optional(),
-    FRONTEND_BASE_URL: z.string().optional(),
+    JWT_SECRET: z.string().default('secret'),
+    SALT_ROUNDS: z.number().default(10),
+    ACCESS_TOKEN_EXPIRY: z.string().default('1h'),
+    REFRESH_TOKEN_EXPIRY: z.string().default('1d'),
+    FRONTEND_BASE_URL: z.string().default('http://localhost:5173'),
     /**
      * Secret key used to bypass authentication, must be unique and rotated if used in production
      */
-    SERVICE_REQUEST_KEY: z.string().optional()
-  },
+    SERVICE_REQUEST_KEY: z.string().optional(),
+    DEPLOYMENT_ENVIRONMENT: z.nativeEnum(Environment).optional()
+  }),
   values: {
-    HOST: process.env.HOST ?? '0.0.0.0',
-    PORT: Number(process.env.SERVICE_PORT ?? process.env.PORT ?? 8080),
-    DB_URL: process.env.DB_URL,
-    REDIS_CONNECTION_STRING: process.env.REDIS_CONNECTION_STRING,
-    JWT_SECRET: process.env.JWT_SECRET ?? 'secret',
-    SALT_ROUNDS: Number(process.env.SALT_ROUNDS || 10),
-    ACCESS_TOKEN_EXPIRY: process.env.ACCESS_TOKEN_EXPIRY ?? '1h',
-    REFRESH_TOKEN_EXPIRY: process.env.REFRESH_TOKEN_EXPIRY ?? '1d',
-    FRONTEND_BASE_URL: process.env.FRONTEND_BASE_URL ?? 'http://localhost:5173',
-    SERVICE_REQUEST_KEY: process.env.SERVICE_REQUEST_KEY
+    ...process.env,
+    CLUSTER_SIZE: process.env.NODE_ENV === 'production' ? process.env.CLUSTER_SIZE : '1',
+    PORT: process.env.SERVICE_PORT ?? process.env.PORT
   }
 });
 
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,24 +1,34 @@
 // prettier-ignore
 import 'dotenv/config';
+import { default as clusterize, type ClusterizerOptions } from '@sliit-foss/clusterizer';
 import { moduleLogger } from '@sliit-foss/module-logger';
 import { default as config } from '@/config';
 import * as database from '@/database/mongo';
-import { app, service } from './app';
 
-const logger = moduleLogger(service);
+const options: ClusterizerOptions = {
+  logger: moduleLogger('Clusterizer'),
+  workers: config.CLUSTER_SIZE
+};
 
-const server = app.listen(config.PORT, config.HOST, () => {
-  logger.info(`${service} listening on ${config.HOST}:${config.PORT}`);
-});
+clusterize((processID) => {
+  const { app, service } = require('./app');
 
-process.on('SIGTERM', () => {
-  logger.info('Received SIGTERM. Server shutdown initiated');
-  server.close(() => {
-    if (process.env.NODE_ENV === 'development') return process.exit(0);
-    logger.info('Server shutdown complete. Exiting after 30 seconds');
-    setTimeout(async () => {
-      await database.disconnect();
-      process.exit(0);
-    }, 30000);
+  const logger = moduleLogger(`${service} >>> pid::${processID}`);
+
+  const server = app.listen(config.PORT, config.HOST, () => {
+    logger.info(`${service} listening on ${config.HOST}:${config.PORT}`);
   });
-});
+
+  if (process.env.NODE_ENV === 'production') {
+    process.on('SIGTERM', () => {
+      logger.info('Received SIGTERM. Server shutdown initiated');
+      server.close(() => {
+        logger.info('Server shutdown complete. Exiting after 30 seconds');
+        setTimeout(async () => {
+          await database.disconnect();
+          process.exit(0);
+        }, 30000);
+      });
+    });
+  }
+}, options);
